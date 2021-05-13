dae7030363a8e03b059c68d3dfceb5a989f4f743
Jan Kurzawski <jk7127@deathvalley.psych.nyu.edu>
2021-05-13 13:19:44 -0400

    cosmetic changes

--- matlab/examples/example_1.m
+++ matlab/examples/example_1.m
@@ -1,14 +1,9 @@
 
-%% addpath to GLMsingle and fracridge
+% addpath to GLMsingle and fracridge
 addpath(genpath('./../'))
-addpath(genpath('/Users/jankurzawski/Documents/fracridge'))
+addpath(genpath('/Users/jk7127/Documents/fracridge'))
 
 clear
-
-if ~exist('./data','dir')
-    mkdir('data')
-    
-end
 
 % Pick dataset
 
@@ -17,16 +12,33 @@ dataset = datasets{2};
 
 % Download data for an example subject
 
+if ~exist('./data','dir')
+    mkdir('data')
+    
+end
+
+
 switch dataset
     
     case 'nsdfloc'
-        !curl -L --output ./data/nsdflocexampledataset.mat https://osf.io/8rmjk/download
+        
+        if ~exist('./data/nsdflocexampledataset.mat','file')
+            !curl -L --output ./data/nsdflocexampledataset.mat https://osf.io/8rmjk/download
+        end
         load('./data/nsdflocexampledataset.mat')
+        
     case 'nsdcore'
-        !curl -L --output ./data/nsdcoreexampledataset.mat https://osf.io/jqpwz/download
+        
+        if ~exist('./data/nsdcoreexampledataset.mat','file')
+            !curl -L --output ./data/nsdcoreexampledataset.mat https://osf.io/jqpwz/download
+        end
         load('./data/nsdcoreexampledataset.mat')
+        
     case 'haxby'
-        !curl -L --output ./data/haxby.mat https://osf.io/f6vpq/download
+        
+        if ~exist('./data/haxby.mat','file')
+            !curl -L --output ./data/haxby.mat https://osf.io/f6vpq/download
+        end
         load('./data/haxby.mat')
 end
 
@@ -49,35 +61,33 @@ fprintf('The sampling rate (TR) is %.6f seconds.\n',tr);
 
 %%
 figure(1);clf
+
 for d = 1 : 4
-    subplot(2,2,d); imagesc(newdesign{d}); colormap gray
+    subplot(2,2,d); imagesc(design{d}); colormap gray; drawnow
     xlabel('Conditions')
     ylabel('TRs')
     title(sprintf('Design matrix for run%i',d))
-    if exist('stimuli','var')
-        xticks([1:length(stimuli)])
-        xticklabels(stimuli)
-        %
-        
-    end
     set(gca,'YDir','normal')
     
 end
 %%
 figure(2);clf
+
 imagesc(makeimagestack(data{1}(:,:,:,1)));
 colormap(gray);
 axis equal tight;
 colorbar;
 title('fMRI data (first volume)');
 %% Call GLMestimatesingletrial using default parameters
-% Outputs will be stored in 'results' and Figures will be written to
-% 'example1figures' in the current directory.
+% Outputs and figures will be stored in folder with experiment name in the
+% current directory or saved to the results variable which is the only
+% output of GLMestimatesingletrial
 
 % default options (all parameters below can be assigned to a variable i.e
 % opt by creating fields (opt.wantlibrary). Options are the 6th input to
 % GLMestimatesingletrial.
 
+% DEFAULT OPTIONS
 % wantlibrary = 1 -> Fit hRF to each voxel
 % wantglmdenoise = 1 -> Use GLMdenoise
 % wantfracridge = 1  -> Use ridge regression to improve beta estimates
@@ -106,7 +116,8 @@ title('fMRI data (first volume)');
 % <HRFindex> is the 1-index of the best HRF, can be retried with
 % getcanonicalhrflibrary(stimdur,tr)
 % <FRACvalue> is the fractional ridge regression regularization level chosen for each voxel
-slice = 1;
+
+slice = 20; % adjust this number when using different datasets
 val2plot = {'meanvol';'R2';'HRFindex';'FRACvalue'};
 cmaps = {gray;hot;parula;copper};
 figure(3);clf
@@ -122,5 +133,42 @@ for v = 1 : length(val2plot)
     
 end
 
-set(gcf,'Position',[ 1224         840         758         408])
+set(gcf,'Position',[1224 840 758 408])
 
+%% Plot single beta weights for one voxel for all conditions
+betas = results{4}.modelmd;
+% locate beta weights for the same condition in beta output. 
+newdesign = design;    
+for r = 1 : length(design)
+    
+    dm = full(design{r});
+    newdm = zeros(size(dm));
+    for cond = 1 : size(dm,2)
+        
+        tmp = dm(:,cond) == 1;
+        newdm(tmp,cond) = cond;
+        newdesign{r} = newdm;
+    end
+end
+ 
+indx_of_beta=cell2mat(cellfun(@flatten,newdesign,'UniformOutput',false));
+indx_of_beta(indx_of_beta==0) = [];
+% index_of_beta should correspond to the 4th dimension of betas
+[maxval,ind] = max(results{4}.R2(:));
+[x,y,z] = ind2sub(size(results{4}.R2),ind);
+
+figure(4);clf
+for i = unique(indx_of_beta)
+    
+    beta_ind = indx_of_beta == i;
+    
+    plot(ones(sum(beta_ind),1)*i,squeeze(betas(x,y,z,beta_ind)),'.','MarkerSize',10); hold on
+    bar(i,nanmedian(squeeze(betas(x,y,z,beta_ind))),'FaceColor','None');
+end
+
+xticks(1:length(unique(indx_of_beta)))
+
+ylabel('Single trial beta weight')
+xlabel('Conditions')
+title('Single beta weights for each condition for voxel with highest R2')
+set(gca,'Fontsize',15)
